0d9c0a29530aa5a3b0d1b5a05333c6bb91fc01af
Sergio Corrente <corrente.sergio@outlook.be>
2020-08-10 09:20:07 +0200

    eerste puch

---
+++ mijnEersteGithb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mijnEersteGithb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
